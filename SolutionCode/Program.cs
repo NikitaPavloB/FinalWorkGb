0c311b3a09e6d98d78b0884ee20ceef9a7811218
Nikita <kindpavlov@yandex.ru>
2023-04-25 14:44:56 +0300

    Написал код для задачи

---
+++ SolutionCode/Program.cs
@@ -0,0 +1,63 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+Console.Clear();
+
+Console.Write("Введите количество строк в массиве: ");
+int n = int.Parse(Console.ReadLine());
+
+string[] array = new string[n];
+for (int i = 0; i < n; i++)
+{
+  Console.Write($"Введите строку {i + 1}: ");
+  array[i] = Console.ReadLine();
+}
+
+string[] result = FilterArray(array);
+
+Console.WriteLine("Исходный массив:");
+PrintArray(array);
+
+Console.WriteLine("Массив с элементами длиной не более 3 символов:");
+PrintArray(result);
+
+
+string[] FilterArray(string[] array)
+{
+  int count = 0;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i].Length <= 3) count++;
+  }
+
+  string[] result = new string[count];
+  int index = 0;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i].Length <= 3)
+    {
+      result[index] = array[i];
+      index++;
+    }
+  }
+
+  return result;
+}
+
+void PrintArray(string[] array)
+{
+  Console.Write("[");
+  for (int i = 0; i < array.Length; i++)
+  {
+    Console.Write($"\"{array[i]}\"");
+    if (i != array.Length - 1)
+    {
+      Console.Write(", ");
+    }
+  }
+  Console.WriteLine("]");
+}
+
+
